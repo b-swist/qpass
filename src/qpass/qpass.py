78f1ed4b2d48d23c77f52512eef26659a323f39a
Bartosz Świst <bswist@protonmail.com>
2026-09-24 18:39:04 +0200

    update cli

--- src/qpass/qpass.py
+++ src/qpass/qpass.py
@@ -5,7 +5,7 @@ VERSION = "1.0.0"
 
 
 def main() -> None:
-    args = parse_args()
+    args, help = parse_args()
     qdk.init(project_root="./.")
 
     match args.command:
@@ -13,18 +13,28 @@ def main() -> None:
             qdk.code.Main.Generate(args.length, args.no_symbols)
         case "check":
             qdk.code.Main.Check(list(args.password))
+        case None:
+            print(help)
 
 
-def parse_args() -> argparse.Namespace:
+def parse_args() -> tuple[argparse.Namespace, str]:
     parser = argparse.ArgumentParser(description="Quantum Password Generator")
     parser.add_argument("-v", "--version", action="version", version=VERSION)
 
-    subparsers = parser.add_subparsers(dest="command", required=True)
+    subparsers = parser.add_subparsers(dest="command")
 
-    generate_parser = subparsers.add_parser("generate")
-    generate_parser.add_argument("-l", "--length", type=int, default=24, help="set generated password length")
-    generate_parser.add_argument("-n", "--no-symbols", action="store_true", help="exclude symbols from the password")
     check_parser = subparsers.add_parser("check", help="check password entropy")
     check_parser.add_argument("password", type=str, help="password to be checke")
 
-    return parser.parse_args()
+    generate_parser = subparsers.add_parser("generate", help="generate new password")
+    generate_parser.add_argument(
+        "-l", "--length", type=int, default=24, help="set password length"
+    )
+    generate_parser.add_argument(
+        "-n",
+        "--no-symbols",
+        action="store_true",
+        help="exclude symbols from the password",
+    )
+
+    return parser.parse_args(), parser.format_help()
